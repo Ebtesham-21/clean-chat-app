dfc350c70468da65a947e6aab0c271d3f40362a8
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-18 16:25:16 +0600

    front end

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,22 @@ import cors from 'cors';
 import path from 'path';
 import prisma from './config/dbclient';
 
+
+const PORT = process.env.PORT || 5000;
 const app: Application = express();
 
 const startServer = async () => {
     try{
         await prisma.$connect();
-    }
+        console.log("Connected to database");
+        const server = app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } 
     
     catch(err){
 
     }
 }
+
+startServer();
